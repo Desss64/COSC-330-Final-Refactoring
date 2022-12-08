2c27a6df06f3e535f0173fa3efeae16a45ba5b28
alexh <47182454+alexhearne@users.noreply.github.com>
2022-12-08 15:08:49 -0500

    Phase 2 Step 10

--- src/Rental.java
+++ src/Rental.java
@@ -22,15 +22,9 @@ public class Rental {
     public double getCharge() {
 		return movie.getCharge(daysRented);
 	}
-    
+    	
     public int getFrequentRenterPoints() {
-		int frequentRenterPoints = 0;
-		// add frequent renter points
-		frequentRenterPoints++;
-		// add bonus for a two day new release rental
-		if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
-				frequentRenterPoints++;
-		return frequentRenterPoints;
+    	return movie.getFrequentRenterPoints(daysRented);
 	}
 
 }
